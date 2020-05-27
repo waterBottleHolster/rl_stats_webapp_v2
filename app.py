bb2f52e0ccf9bfef6950a8c68a72929573dd0745
waterBottleHolster <kirk-mcmillan@hotmail.com>
2020-05-26 19:40:55 -0700

    added buttload of images, working on changing color scheme for correct team now

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ def index():
 def home():
     return redirect("/")
 
+@app.route("/pre-game")
+def pregame_settings():
+    return render_template("pre_game_settings.html")
+
 @app.route("/camera-settings")
 def camera_settings():
     return render_template("camera_settings_v2.html")
