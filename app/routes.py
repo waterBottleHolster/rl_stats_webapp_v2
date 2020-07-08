94bba0dab4362f91fff13e1e0f8bf8209d775f2a
waterBottleHolster <kirk-mcmillan@hotmail.com>
2020-07-08 04:55:00 -0700

    big update; flash messages are formatted, flask-login installed, login screen made pretty again after flask-wtf implementation, login data validation.

--- app/routes.py
+++ app/routes.py
@@ -5,24 +5,12 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index/')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html')
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        flash('Login requested for user {}'.format(form.username.data))
         return redirect('/index/')
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', form=form)
