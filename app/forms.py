f77f3b98063cff4368891fbcae08b1989a432ceb
riverOfGold <60319725+riverOfGold@users.noreply.github.com>
2020-07-11 18:06:24 -0700

    added correct she bang for dreamhost server

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,4 @@
+#!/home/dh_4gxtme/rl-experiment-tracker.com/public/rl_stats_webapp_v2/venv_rl_webapp/bin/python3
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
