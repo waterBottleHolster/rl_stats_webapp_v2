44e2069b7a795d825cf8544014dfedd104270964
waterBottleHolster <kirk-mcmillan@hotmail.com>
2020-09-12 19:14:15 -0700

    added flask_migrate to rl_stats_site.py and started making the factory function in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,24 @@
 #!/home/dh_4gxtme/rl-experiment-tracker.com/public/rl_stats_webapp_v2/.flask_venv/bin/python3
-from flask import Flask
-from config import Config
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from config import Config
 
-app = Flask(__name__)
-login = LoginManager(app)
+login_manager=LoginManager()
+login_manager.login_view = 'login'
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    login_manager.init_app(app)
+    db.init_app(app)
+
+    #attach routes and custom error pages here
+
+    return app
+#login = LoginManager(app)
 login.login_view = 'login'
-app.config.from_object(Config)
 
 from app import routes
--- rl_stats_site.py
+++ rl_stats_site.py
@@ -1,1 +1,12 @@
-from app import app
+import os
+from app import create_app, db
+from app.models import User, Role, game_event
+from flask_migrate import Migrate
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+# I'm not sure what the following lines do
+#@app.shell_context_processor
+#def make_shell_context():
+#    return dict(db=db, User=User, Role=Role)
